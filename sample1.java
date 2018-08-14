a4e4e0766952228a237633cc900d6e2d75249850
suprajamai <41013810+suprajamai@users.noreply.github.com>
2018-08-14 20:23:10 +0530

    Update sample1.java

--- sample1.java
+++ sample1.java
@@ -8,6 +8,8 @@ Scanner s=new Scanner(System.in);
 c=s.next().charAt(0);
 if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
   System.out.println("Vowel");
+else if(!(c>='a' && c<='z')||(c>='A' && c<='Z'))
+  System.out.println("invalid");
 else
   System.out.println("consonant");
 s.close();
