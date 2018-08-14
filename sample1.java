3cc6ace0cdba02adf7372c60f6f5a933b782e4af
suprajamai <41013810+suprajamai@users.noreply.github.com>
2018-08-14 20:14:47 +0530

    Create sample1.java

---
+++ sample1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class sample1
+{
+public static void main(String args[])
+{
+char c;
+Scanner s=new Scanner(System.in);
+c=s.next().charAt(0);
+if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+  System.out.println("Vowel");
+else
+  System.out.println("consonant");
+s.close();
+}
+}
