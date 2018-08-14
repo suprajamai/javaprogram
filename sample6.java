d6e7150bce8d5ccc2d120d8b04f626a86fb5d478
suprajamai <41013810+suprajamai@users.noreply.github.com>
2018-08-14 21:45:03 +0530

    Create sample6.java

---
+++ sample6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sample6
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n%4==0)
+System.out.println("yes");
+else
+System.out.println("no");
+s.close();
+}
+}
